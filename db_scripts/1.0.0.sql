49453993b459877da6a34aea934be866b482c990
刘述 <383883040@qq.com>
2020-09-19 16:45:47 +0800

    提交代码

--- db_scripts/1.0.0.sql
+++ db_scripts/1.0.0.sql
@@ -1,7 +1,14 @@
 create table Teacher(
-  id int not null auto_increment primary key,
-  name varchar(10) not null comment '姓名',   
-  sex char(1) not null comment '性别',
-  age tinyint(100) not null comment '年龄',
+  id                        int                 not null      comment '主键Id',
+  name                      varchar(10)         not null      comment '姓名',   
+  age                       tinyint(100)        not null      comment '年龄',
+  sex                       char(1)             not null      comment '性别',
+  positional_titles         char(10)            null          comment '职称',
+  person_id                 char(18)            not null      comment '身份证号码',
+  graduate_school           char(50)            not null      comment '毕业院校',
+  speciality                char(50)            not null      comment '专业',
+  studies_time              date                null          comment '进修时间',
+  work_start_time           date                not null      comment '工作时间',
+  
   primary key (id)
-)comment = '学生表';
+) comment = '学生表';
