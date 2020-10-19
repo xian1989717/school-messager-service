b6791f461becedeee7e941e13798388370434b28
刘述 <383883040@qq.com>
2020-10-19 18:54:15 +0800

    提交代码

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa')
+const cors = require('koa2-cors')
+const bodyParser = require('koa-bodyparser')
+
 const {
   teacherRouter
 } = require('./router/index')
 
 const app = new Koa()
 
-const bodyParser = require('koa-bodyparser')
-
+app.use(cors())
 app.use(bodyParser())
 
 app
