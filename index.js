baa364aecb4edd6c3a41da1cbcad33905be38b45
刘述 <383883040@qq.com>
2020-10-24 17:20:30 +0800

    提交代码

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const cors = require('koa2-cors')
 const bodyParser = require('koa-bodyparser')
+const compress = require('koa-compress')
 
 const {
   teacherRouter
@@ -8,6 +9,7 @@ const {
 
 const app = new Koa()
 
+app.use(compress({ threshold: 1024 }))
 app.use(cors())
 app.use(bodyParser())
 
